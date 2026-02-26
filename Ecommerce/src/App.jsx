d7aca5db517763597dd7ea338eff46a657b02292
Oyinade247 <adedokunoyinade@gmail.com>
2026-02-26 11:34:11 +0100

    Booklist

---
+++ Ecommerce/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import router from './router/routes'
+import { RouterProvider } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <RouterProvider router = {router} />  
+    </>
+  )
+}
+
+export default App
--- Redux/src/components/Counter.jsx
+++ Redux/src/components/Counter.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement , reset} from "../slices/CounterSlice";
+
 
 const Counter = ()=>{
+    const counter = useSelector((state) => state.Counter.count);
+    const dispatch = useDispatch();
     return(
-        <div>Counter</div>
+        <div>
+            <span>{counter}</span>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>reset</button>
+
+        </div>
     )
 }
 
