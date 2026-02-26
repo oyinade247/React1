5e350403c4c8cac68309cf18b49f94be290bb5ba
Your Name <you@example.com>
2026-02-26 15:22:03 +0100

    Introduced store and slice

--- Ecommerce/src/store/Store.jsx
+++ Ecommerce/src/store/Store.jsx
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
+import {productApi} from "../apis/ProductApi"
 
 const store = configureStore({
     reducer: {
-      
-    }
+
+      [productApi.reducerPath]:productApi.reducer
+    },
+    middleware:(getDefaultMiddleWare)=>
+        getDefaultMiddleWare().concat(productApi.middleware)
 })
 
 export default store;
